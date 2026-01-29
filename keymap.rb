898f7678d9e59a76767a747b3a7f38cec9b5fe3f
Akihiro Kurotani <kurod1492@gmail.com>
2026-01-29 10:48:41 +0900

    customize threshold time and keymap

--- keymap.rb
+++ keymap.rb
@@ -13,18 +13,18 @@ kbd.init_pins(
 # default layer should be added at first
 kbd.add_layer :default, %i(
   KC_Q      KC_W      KC_E      KC_R      KC_T       KC_Y       KC_U      KC_I      KC_O      KC_P
-  A_LCTL    KC_S      KC_D      KC_F      KC_G       KC_H       KC_J      KC_K      KC_L      MINS_RCTL
+  KC_A      KC_S      KC_D      KC_F      KC_G       KC_H       KC_J      KC_K      KC_L      KC_MINS
   Z_LSFT    X_LCTL    C_LALT    V_LGUI    SPC_LOWER  ENT_RAISE  B_RGUI    N_RALT    M_RCTL    BS_RSFT
 )
 kbd.add_layer :lower, %i(
   KC_1      KC_2      KC_3      KC_4      KC_5       KC_6       KC_7      KC_8      KC_9      KC_0
   KC_TAB    KC_SCOLON KC_PIPE   KC_COLON  KC_QUOTE   KC_QUES    KC_COMMA  KC_DOT    KC_TILD   KC_SLASH
-  UNDO      CUT       COPY      PASTE     LOWER      KC_LANG1   KC_RGUI   KC_RALT   KC_RCTL   KC_RSFT
+  UNDO      CUT       COPY      PASTE     LOWER      KC_LANG1   KC_RGUI   KC_RALT   KC_RCTL   KC_EQUAL
 )
 kbd.add_layer :raise, %i(
   KC_EXLM   KC_AT     KC_HASH   KC_DLR    KC_PERC    KC_CIRC    KC_AMPR   KC_ASTER  KC_LPRN   KC_RPRN
   KC_ESCAPE KC_LBRC   KC_RBRC   KC_PLUS   KC_DQUO    KC_LEFT    KC_DOWN   KC_UP     KC_RIGHT  KC_BSLS
-  KC_LSFT   KC_LCTL   KC_LALT   KC_LGUI   ADJUST     RAISE      KC_GRAVE  KC_LABK   KC_RABK   KC_EQUAL
+  KC_LSFT   KC_LCTL   KC_LALT   KC_LGUI   ADJUST     RAISE      KC_GRAVE  KC_LABK   KC_RABK   KC_RSFT
 )
 kbd.add_layer :adjust, %i(
   KC_F1     KC_F2     KC_F3     KC_F4     KC_F5      KC_F6      KC_F7     KC_F8     KC_F9     KC_F10
@@ -50,8 +50,8 @@ kbd.define_mode_key :A_LCTL,    [ :KC_A,                               :KC_LCTL,
 kbd.define_mode_key :B_RGUI,    [ :KC_B,                               :KC_RGUI,                   550,             200 ]
 kbd.define_mode_key :B_RCTL,    [ :KC_B,                               :KC_RCTL,                   550,             200 ]
 kbd.define_mode_key :B_RAISE,   [ :KC_B,                               :raise,                     550,             200 ]
-kbd.define_mode_key :N_RALT,    [ :KC_N,                               :KC_RALT,                   550,             200 ]
-kbd.define_mode_key :M_RCTL,    [ :KC_M,                               :KC_RCTL,                   550,             200 ]
+kbd.define_mode_key :N_RALT,    [ :KC_N,                               :KC_RALT,                   200,             200 ]
+kbd.define_mode_key :M_RCTL,    [ :KC_M,                               :KC_RCTL,                   200,             200 ]
 kbd.define_mode_key :M_RGUI,    [ :KC_M,                               :KC_RGUI,                   550,             200 ]
 kbd.define_mode_key :M_RSFT,    [ :KC_M,                               :KC_RSFT,                   550,             200 ]
 kbd.define_mode_key :MINS_RSFT, [ :KC_MINUS,                           :KC_RSFT,                   550,             200 ]
